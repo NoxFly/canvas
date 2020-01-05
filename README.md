1e91a7a07907f2a11868651c9cd85b1535d2bb2d
NoxFly <dorian.thivolle@gmail.com>
2020-01-05 21:05:54 +0100

    version 1.1.0 #firstRelease

--- README.md
+++ README.md
@@ -23,6 +23,10 @@ You must include this file before all other scripts that will use something abou
 
 You will get some predefined variables, you can use some of them, but some others are considered as private and you should not change them, in any case not without a function which has been provided for.
 
+If you have created some examples with this library, send me them so I'll surely add these examples on the github repo (+ credit).
+
+version 1.1.0
+
 ## Create a canvas
 
 To create a new canvas, you must use the function `createCanvas`.
@@ -50,12 +54,66 @@ let my_canvas = createCanvas(canvas_width, canvas_height, background='#000', req
 */
 ```
 
+You can change the resolution of your canvas with `setHTMLview(width, height)`.
+
+Example: you can have an HTML canvas of 500x500, but with a resolution of 2000x2000.
+
+You will have a lower quality of rendering, but you will have more pixel data.
+
+```js
+createCanvas(2000,2000); // canvas contains 2000x2000 pixels
+setHTMLview(500,500); // user have a 500x500 canvas
+```
+
+To switch between the two resolution's values, you can use the following functions:
+
+```js
+// all these functions return either a x, y or both
+rendering(vector); // with a vector as parameter
+rendering(x, y); // with x,y values
+renderingX(x); // only for a x value
+renderingY(y); // only for a y value
+
+// for example
+createCanvas(2000,2000);
+setHTMLview(500,500);
+
+// you enter a HTMLview value -> return a canvas resolution value
+console.log(rendering(250,500)); // 1000,2000
+```
+
 ## The 2 basics functions
 
 You can animate your canvas easily. Thanks for these 2 functions which will be executed automatically once your declare them:
 
 * `function setup(void) {}`: This function is executed __before__ the `draw` function, but __after__ the window's loading. So you have access to the `document` variable in.
 * `function draw(void) {}`: This function is executed __after__ the `setup` function and the window's loading. `requestAnimationFrame(draw)` is called, so the draw function is looping. Otherwise, you can set a condition to stop the requestAnimationFrame. While this condition return false, the draw loop will not be executed. Thanks for the function `setDrawCondition(condition)`.
+
+```js
+function setup() {
+    // init your vars here
+    createCanvas(500,500);
+}
+
+function draw() {
+    // draw loop
+    fill('white');
+    line(0,0,500,500);
+}
+```
+
+## Guidelines
+
+You can show the h/v blue guidelines (dev tool) to show you the x/y pixel you mouse is hovering.
+
+```js
+function setup() {
+    createCanvas(500,500);
+    showGuideLines(true); // by default false
+}
+```
+
+![Guidelines screenshot](https://www.zupimages.net/up/20/01/i3re.png)
 
 ## Public variables
 * `MIN_DOC_SIZE`: The value of the minimum between the document's width / height
@@ -67,6 +125,8 @@ You can animate your canvas easily. Thanks for these 2 functions which will be e
 * `ctx`: you can access to it, but do not modify it. if you didn't create the canvas yet, its value is `null`.
 * `width`: the width of the created canvas. Please do not modify it.
 * `height`: the height canvas. Please do not modify it.
+* `realWidth`: the real width of your canvas if you used `setHTMLview`
+* `realHeight`: the real height of your canvas if you used `setHTMLview`
 * `mouseX`: it's the mouse's X position relative to the canvas
 * `mouseY`: it's the mouse's Y position relative to the canvas
 
@@ -84,16 +144,17 @@ It can be a vector for acceleration, speed, position,...
 
 ```js
 let object = new Vector(0,0); // position of the object: x=0; y=0
-
 object.set(10,50); // finally we want to move the object to 10;50
-
 object.add(10,50) // 10,50 + 10,50 = 20,100 -> now object has position x=20 and y=100
-
 let mag = object.mag; // returns the magnitude of the vector
-
 object.setMag(10); // changes the object's magnitude
-
 object.normalize(); // adapt values of the object between 0 and 1
+
+// access to x and y properties
+console.log(object.x, object.y);
+
+// to get a string value
+console.log(object.toString()); // '{x: 2, y: 6}'
 ```
 
 ## Basic Functions to draw shapes
@@ -139,9 +200,15 @@ text(txt, x, y);
 ### Format the text
 
 ```js
-setFont(fontSize, font);
 // must specify the font size unity, example:
 // setFont('24px', 'Monospace');
+setFont(fontSize, font);
+
+// to change only the size
+setFontSize(size);
+
+// to change only the font family
+setFontFamily(font);
 
 alignText(alignment); // left, right, center, start or end. default is left
 ```
@@ -167,16 +234,25 @@ translate(x, y); // translates the canvas
 rotate(degree); // rotates the canvas
 pop(); // restores the canvas before the translation & rotation
 clear(); // clears the canvas draw. Normally you don't have to use it, because the draw() function do
+
+background(color); // change the canvas's background
 ```
 
 ## Mathematical functions
 
 ```js
 radians(degree); // returns the radian's value of a degree one
+degree(radian); // returns the degree's value of a radian one
 angleToVector(angle); // returns the vector of a degree
 dist(a, b); // returns the distance between two vectors
 map(val, start1, end1, start2, end2); // range mapping a value
 random(max); // returns a random int from 0 to max-1
+pow(n); // returns n²
+sqrt(n); // return the sqrt of n
+abs(n); // return the absolute of n
+hex(r, g, b); // return the hexadecimal value of the rgb one for 3 given integers
+hexs(rgb); // same but for a string : "rgb(255,255,255)"
+rgb(hexa); // return the rgb value as a string for a given hexadecimal value
 ```
 
 ## Mouse Properties
@@ -226,6 +302,14 @@ function mouseLeave(e) {
 function mouseWheel(e) {
     // called when the mouse wheel is used on the canvas
 }
+
+function mouseDown(e) {
+    // called when the mouse is pressed on the canvas
+}
+
+function mouseUp(e) {
+    // called when the mouse is released
+}
 ```
 
 ## Keyboard properties
@@ -252,6 +336,124 @@ function keyUp(e) {
     // called when a key is up
 }
 ```
+
+## Advanced shapes
+
+To create a new shape, you can use the class `Shape`.
+
+However, this one has all common default parameters for all type of shapes,
+
+so I recommend to not use this class. Use which will follow this one.
+
+### what contains a shape
+
+* a fill color. You can :
+    * acces to it doing `shape.background`
+    * modify it doing `shape.fill = 'red'`
+* a stroke color. You can :
+    * acces to / modify it doing `shape.stroke`
+    * change its weight / get its weight using `shape.strokeWeight`
+* a position (x,y).
+    * You can access to them thanks `shape.x` and `shape.y`.
+    * You can modify the position thanks `shape.setPosition(x, y)` or `shape.setPosition(vector)`
+* a speed (by default 0, it's a static shape). You can acces to / modify it using `shape.speed`
+* an acceleration (by default 0).
+    * You can access to / modify it using `shape.acceleration`
+    * In the case of the shape is running, it will move with `this.speed + this.acceleration`, else only `this.speed`
+* to know if the shape is running, use `shape.running` (returns a boolean)
+* to say either the shape is running or not, use `shape.run(bool)`
+* to move a shape thanks its speed, you can do `shape.move(x,y)`. Be careful, this x and y are the vector direction of the move.
+
+For example:
+```js
+shape.speed = 5;
+shape.move(1,0); // will move to 5 pixels to the right per frame. Write it on draw() function to move it continually
+// you can also put a vector as parameter
+```
+
+### the shape origin
+
+The origin of the shape is the (x,y) of this one.
+
+It can be a different one by default for each shape, but you can change it.
+
+You can also choose if you want to show it or not (a small green circle).
+
+```js
+// says if it shows the origin of the shape
+shape.showOrigin(bool);
+
+// set a new origin
+setOrigin(newOrigin);
+/* Possible origins :
+    topLeft, top, topRight
+    left, center, right
+    bottomLeft, bottom, bottomRight
+*/
+```
+
+To draw a shape, use `shape.draw()`
+
+You an also listen if you are hover the shape:
+
+```js
+if(shape.hover()) {
+    shape.fill = 'red';
+}
+```
+
+### Collisions
+
+To know if two shapes are in collision, use `collision(shape1, shape2)` function.
+
+```js
+if(collision(my_rectangle, my_circle)) {
+    my_circle.fill = 'red';
+    my_rectangle.fill = 'red';
+}
+```
+
+### RectangleShape
+```js
+let rect = new RectangleShape(x, y, width, height, fill='black', stroke='transparent', strokeWeight=1);
+```
+
+* get/set its width with `shape.width`
+* get/set its height with `shape.height`
+* default origin is `topLeft`
+
+### CircleShape
+```js
+let circ = new CircleShape(x, y, r, fill='black', stroke='transparent', strokeWeight=1);
+```
+
+* get/set its radius thanks `shape.r`
+* default origin is `center`
+
+### TriangleShape
+```js
+let tri = new TriangleShape(x, y, baseLength, baseTilt, height, heightPosition, fill='black', stroke='transparent', strokeWeight=1);
+// the baseTilt is in degree
+// the heightPosition is relative to x, so
+//  - heightPosition =  0 = x
+//  - heightPosition = -1 = x-1
+```
+
+* get its points thanks `shape.A`, `shape.B` and `shape.C`. It's vectors. You cannot modify them
+* get/set its base length thanks `shape.baseLength`
+* get/set its base tilt thanks `shape.baseTilt` (in radian)
+* get/set its height thanks `shape.height`
+* get/set its height position thanks `shape.heightPosition`
+
+**For now you cannot change the origin of the triangle (bottomLeft = A point).**
+
+### Triangle
+```js
+let tri = new Triangle(x1,y1, x2,y2, x3,y3, fill='black', stroke='transparent', strokeWeight=1);
+// it's a TriangleShape but with another constructor
+```
+
+same properties as `TriangleShape`.
 
 ## License
 
--- canvas.js
+++ canvas.js
@@ -1,29 +1,37 @@
 /**
  * @copyright   Copyright (C) 2019 - 2020 Dorian Thivolle All rights reserved.
  * @license     GNU General Public License version 3 or later; see LICENSE.txt
- */
+*/
 
- // public vars
-let ctx = null, canvas = null, width = 0, height = 0;
+// public vars
+let ctx = null, canvas = null, width = 0, height = 0, realWidth = 0, realHeight = 0;
 let mouseX = 0, mouseY = 0;
 let MIN_DOC_SIZE;
-
-// private vars
-let bFill = true, bStroke = true;
-let keys = {};
-let oldMouseX = 0, oldMouseY = 0;
-let isPointerLocked = false;
-let mouseDirection = {x: 0, y: 0};
-let swipexDown = null;
-var swipeyDown = null;
-let swipePCEnable = true;
-let lastSwipe = null;
-
 let isDevice = {
 	mobile: /iPhone|iPad|iPod|Android/i.test(navigator.userAgent),
 	ios: /iPad|iPhone|iPod/.test(navigator.userAgent),
 	android: /Android/.test(navigator.userAgent)
 };
+let originArr = [
+	'topLeft',    'top',    'topRight',
+	'left',       'center', 'right',
+	'bottomLeft', 'bottom', 'bottomRight'
+];
+let fontSize = "12px";
+let fontFamily = "Monospace";
+
+// private vars
+let bFill = true, bStroke = true;
+let keys = {};
+let isMouseDown = false;
+let oldMouseX = 0, oldMouseY = 0;
+let isPointerLocked = false;
+let mouseDirection = {x: 0, y: 0};
+let swipexDown = null;
+let swipeyDown = null;
+let swipePCEnable = true;
+let lastSwipe = null;
+let bGuideLines = false;
 
 
 const line = (x1,y1,x2,y2) => {
@@ -41,20 +49,21 @@ function polyline() {
 	}
 
 	ctx.beginPath();
+		if(arguments.length > 0) {
+			ctx.moveTo(arguments[0], arguments[1]);
+		}
 
-	if(arguments.length > 0) {
-		ctx.moveTo(arguments[0], arguments[1]);
-	}
+		for(let i=2; i < arguments.length; i+=2) {
+			let x = arguments[i],
+				y = arguments[i+1];
+			
+			ctx.lineTo(x,y);
+		}
 
-	for(let i=2; i < arguments.length; i+=2) {
-		let x = arguments[i],
-			y = arguments[i+1];
-		
-		ctx.lineTo(x,y);
-	}
+	ctx.closePath();
 
 	if(bStroke) ctx.stroke();
-	ctx.closePath();
+	if(bFill) ctx.fill();
 }
 
 const circle = (x, y, r) => {
@@ -73,8 +82,18 @@ const rect = (x, y, w, h) => {
 	if(bStroke) ctx.stroke();
 };
 
-const text = (txt, x, y) => {ctx.fillText(txt, x, y);};
-const setFont = (fontSize, font) => {ctx.font = `${fontSize} ${font}`;};
+const text = (txt, x, y) => {
+	if(/<br>/.test(txt)) {
+		let size = fontSize.replace(/(\d+)(\w+)?/, '$1');
+		txt = txt.split('<br>');
+		for(let i=0; i<txt.length; i++) ctx.fillText(txt[i], x, y+i*size);
+	} else {
+		ctx.fillText(txt, x, y);
+	}
+};
+const setFont = (size, font) => {ctx.font = `${size} ${font}`; fontSize=size; fontFamily=font};
+const setFontSize = size => {ctx.font = `${size} ${fontFamily}`; fontSize=size};
+const setFontFamily = font => {ctx.font = `${fontSize} ${font}`; fontFamily=font};
 const alignText = alignment => {ctx.textAlign = ['left', 'right', 'center', 'start', 'end'].indexOf(alignment) > -1? alignment : 'left';};
 
 // push & pop & translate
@@ -87,25 +106,57 @@ const rotate =		degree	=> {ctx.rotate(radians(degree))};
 const noFill = 		() => {bFill = false;};
 const noStroke = 	() => {bStroke = false;};
 
+const background = color => {canvas.style.backgroundColor = color;};
 
 // calculs & parameters
-const stroke = 			color 	=> 	{ctx.strokeStyle = color; bStroke = true;};
-const strokeWeight =	weight	=>	{ctx.lineWidth = weight;};
-const linecap =			style	=>	{ctx.lineCap = ['butt','round','square'].indexOf(style) > -1? style : 'butt';};
-const fill = 			color 	=> 	{ctx.fillStyle = color; bFill = true;};
-const clear = 			() 		=> 	{ctx.clearRect(0,0,canvas.width,canvas.height);}
-const radians = 		deg 	=> 	deg * (Math.PI/180);
-const angleToVector = 	angle 	=> 	new Vector(Math.cos(angle), Math.sin(angle));
-const random = 			max 	=> 	Math.floor(Math.random() * max);
-const dist = 			(a,b) 	=> 	Math.hypot(a.x-b.x, a.y-b.y);
+const stroke = 			color 	=> {ctx.strokeStyle = color; bStroke = true;};
+const strokeWeight =	weight	=> {ctx.lineWidth = weight;};
+const linecap =			style	=> {ctx.lineCap = ['butt','round','square'].indexOf(style) > -1? style : 'butt';};
+const fill = 			color 	=> {ctx.fillStyle = color; bFill = true;};
+const clear = 			() 		=> {ctx.clearRect(0,0,canvas.width,canvas.height);}
+const radians = 		deg 	=> deg * (Math.PI/180);
+const degree = 			rad 	=> rad * (180/Math.PI);
+const angleToVector = 	angle 	=> new Vector(Math.cos(angle), Math.sin(angle));
+const random = 			max 	=> Math.floor(Math.random() * max);
+const dist = 			(a,b) 	=> Math.hypot(a.x-b.x, a.y-b.y);
 const map =				(val, start1, end1, start2, end2) => ((val-start1)/(end1-start1))*(end2-start2)+start2;
+const pow =				n		=> n*n;
+const abs =				n		=> (n >= 0)? n : -n;
+const sqrt =			n		=> Math.sqrt(n);
+function min			() 		{return Math.max(...arguments);}
+function max			() 		{return Math.min(...arguments);}
+
+const hex = (r,g,b) => {   
+	r = Number(r).toString(16); if(r.length < 2) r = "0"+r;
+	g = Number(g).toString(16); if(g.length < 2) g = "0"+g;
+	b = Number(b).toString(16); if(b.length < 2) b = "0"+b;
+	return "#"+r+g+b;
+};
+
+const hexs = (rgb)=> {
+	let r = parseInt(rgb.replace(/rgba?\((\d+),\d+,\d+(\,\d+)?\)/,'$1'));
+	let g = parseInt(rgb.replace(/rgba?\(\d+,(\d+),\d+(\,\d+)?\)/,'$1'));
+	let b = parseInt(rgb.replace(/rgba?\(\d+,\d+,(\d+)(\,\d+)?\)/,'$1'));
+	return hex(r,g,b);
+};
+
+const rgb =	hexa => {
+	hexa = hexa.replace('#','');
+	if(hexa.length == 3) hexa = hexa[0]+hexa[0]+hexa[1]+hexa[1]+hexa[2]+hexa[2];
+	return 'rgb('+parseInt(hexa.substring(0,2), 16)+','+parseInt(hexa.substring(2,4), 16)+','+parseInt(hexa.substring(4,6), 16)+')';
+};
 
 // get last swipe direction
 const getSwipe = () => lastSwipe;
 
 // key event
 const isKeyDown = keyCode => keys[keyCode];
-const isKeyUp = keyCode => !keys[keyCode];
+const isKeyUp 	= keyCode => !keys[keyCode];
+
+// scale for rendering
+const rendering  = (x, y=null) 	=> new Vector(((x instanceof Vector && !y)? x.x:x) * width/realWidth, ((x instanceof Vector && !y)?x.y:y) * height/realHeight);
+const renderingX = x 			=> x*width/realWidth;
+const renderingY = y 			=> y*height/realHeight;
 
 const mouseDir = () =>
 	isPointerLocked?
@@ -123,7 +174,7 @@ const mouseDir = () =>
 
 
 const enablePCswipe = bool => {
-	typeof bool == "boolean"? swipePCEnable = bool : swipePCEnable = true;
+	swipePCEnable = typeof bool == "boolean"? bool : true;
 
 	if(swipePCEnable) {
 		document.removeEventListener('mousedown', handleTouchStart, false);
@@ -134,6 +185,16 @@ const enablePCswipe = bool => {
 	}
 };
 
+function setHTMLview(w, h) {
+	if(w < 0 || h < 0) return;
+	realWidth = w;
+	realHeight = h;
+	if(canvas) {
+		canvas.style.width = realWidth;
+		canvas.style.height = realHeight;
+	}
+}
+
 // MAIN
 function createCanvas(w, h, bg="#000", requestPointerLock=false) {
 	if(canvas != null) {
@@ -143,9 +204,21 @@ function createCanvas(w, h, bg="#000", requestPointerLock=false) {
 	}
 
 	canvas = document.createElement('canvas');
+
+	width = w;
+	height = h;
+
+	canvas.width = width;
+	canvas.height = height;
+
+	if(realWidth == 0) {
+		realWidth = width;
+		realHeight = height;
+	}
+
 	canvas.id = "dynamic-canvas";
-	canvas.width = w;
-	canvas.height = h;
+	canvas.style.width = realWidth;
+	canvas.style.height = realHeight;
 	canvas.style.background = bg;
 	document.body.appendChild(canvas);
 
@@ -165,32 +238,41 @@ function createCanvas(w, h, bg="#000", requestPointerLock=false) {
 		};
 	}
 
-	width = w;
-	height = h;
-
 	ctx = canvas.getContext('2d');
 	
 	return canvas;
 }
 
+function showGuideLines(bool) {
+	bGuideLines = typeof bool == 'boolean'? bool : false;
+}
+
 let drawCond = () => true;
 
 function setDrawCondition(condition = null) {
-	if(condition) {
-		drawCond = condition;
-	}
+	if(condition) drawCond = condition;
 }
 
 function drawLoop() {
     if(ctx && typeof draw != "undefined" && drawCond()) {
         clear();
-        draw();
+		draw();
+		if(bGuideLines) {
+			fill('#46eaea');
+			stroke('#46eaea');
+			strokeWeight(1);
+			line(0,mouseY,width,mouseY);
+			line(mouseX,0,mouseX,height);
+			text(`${Math.floor(mouseX)},${Math.floor(mouseY)}`,mouseX+5,mouseY-5);
+		}
     }
 
     requestAnimationFrame(drawLoop);
 }
 
 function handleTouchStart(e) {
+	isMouseDown = true;
+	if(typeof mouseDown != "undefined") mouseDown(e);
 	let getTouches = e2 => e2.touches || [{clientX: e.clientX, clientY: e.clientY}, null];
 
 	const firstTouch = getTouches(e)[0];
@@ -199,6 +281,7 @@ function handleTouchStart(e) {
 }
 
 function handleTouchMove(e) {
+	if(typeof mouseDown != "undefined" && isMouseDown) mouseDown(e);
 	if(!swipexDown || !swipeyDown) {
 		return;
 	}
@@ -233,6 +316,38 @@ function handleTouchMove(e) {
 	swipeyDown = null;
 }
 
+function getOffsetVector(shape) {
+	if(!(shape instanceof Shape)) {
+		console.error('Argument must be a Shape type');
+		return new Vector(0, 0);
+	}
+
+	let vec = new Vector(0, 0), w, h, o;
+
+	if(shape instanceof RectangleShape) {
+		w = shape.width/2; h = shape.height/2, o = new Vector(0,0);
+	}
+
+	else if(shape instanceof CircleShape) {
+		w = shape.r; h = w, o = new Vector(shape.r, shape.r);
+		vec.set(shape.r, shape.r);
+	}
+	
+	switch(shape.origin) {
+		case 'topLeft':		vec.set(-o.x, -o.y);		break;
+		case 'top': 		vec.set(w-o.x, -o.y); 		break;
+		case 'topRight': 	vec.set(w*2-o.x, -o.y); 	break;
+		case 'left': 		vec.set(-o.x, h-o.y); 		break;
+		case 'center': 		vec.set(w-o.x, h-o.y); 		break;
+		case 'right': 		vec.set(w*2-o.x, h-o.y); 	break;
+		case 'bottomLeft': 	vec.set(-o.x, h*2-o.y); 	break;
+		case 'bottom': 		vec.set(w-o.x, h*2-o.y); 	break;
+		case 'bottomRight': vec.set(w*2-o.x, h*2-o.y);
+	}
+
+	return vec;
+}
+
 window.onload = e => {
 	MIN_DOC_SIZE = Math.min(document.documentElement.clientHeight, document.documentElement.clientWidth);
 	
@@ -258,6 +373,7 @@ window.onload = e => {
 
 		canvas.addEventListener('touchstart', handleTouchStart, false);
 		canvas.addEventListener('touchmove',  handleTouchMove, false);
+		canvas.addEventListener('mouseup', e => {isMouseDown = false; if(typeof mouseUp != "undefined") mouseUp(e);});
 
 		if(swipePCEnable) {
 			canvas.addEventListener('mousedown', handleTouchStart, false);
@@ -304,7 +420,7 @@ class Vector {
 	}
 
 	normalize() {
-		var norm = Math.sqrt(this.x * this.x + this.y * this.y);
+		let norm = Math.sqrt(this.x * this.x + this.y * this.y);
 		if (norm != 0) {
 			this.x = this.x / norm;
 			this.y = this.y / norm;
@@ -329,4 +445,252 @@ class Vector {
 		this.x = this.x * newMag / this.mag;
 		this.y = this.y * newMag / this.mag;
 	}
-};
+
+	toString() {
+		return `{x: ${this.x}, y: ${this.y}}`;
+	}
+}
+
+function collision(shape1, shape2) {
+	if(!(shape1 instanceof Shape && shape2 instanceof Shape)) {
+		console.error('Collision is only for 2 Shape types');
+		return undefined;
+	}
+
+	function colRaR(a, b) {
+		let x2 = x1 + a.width, y2 = y1 + a.height,
+			x4 = x3 + b.width, y4 = y3 + b.height;
+		return 	x1 < x4 && x2 > x3 && y1 < y4 && y2 > y3;
+	}
+
+	function colRaC(r, c) {
+		offset1 = getOffsetVector(r);
+		offset2 = getOffsetVector(c);
+
+		let cx = c.x - offset2.x, cy = c.y - offset2.y,
+			rx = r.x - offset1.x, ry = r.y - offset1.y;
+
+		let testX = (cx<rx)? rx : (cx>rx+r.width)? rx + r.width : cx,
+			testY = (cy<ry)? ry : (cy>ry+r.height)? ry + r.height : cy;
+
+		return sqrt(pow(cx - testX) + pow(cy - testY)) <= c.r;
+	}
+
+	function colCaC(c1, c2) {
+		let dx = x1 - x3, dy = y1 - y3;
+		return sqrt(dx * dx + dy * dy) < c1.r + c2.r;
+	}
+
+	let offset1 = getOffsetVector(shape1),
+		offset2 = getOffsetVector(shape2);
+
+	let x1 = shape1.x - offset1.x, y1 = shape1.y  - offset1.y,
+		x3 = shape2.x - offset2.x, y3 = shape2.y - offset2.y;
+
+	if(shape1 instanceof RectangleShape) {
+		if(shape2 instanceof RectangleShape) 	return colRaR(shape1, shape2)
+		else if(shape2 instanceof CircleShape) 	return colRaC(shape1, shape2)
+	} else if(shape1 instanceof CircleShape) {
+		if(shape2 instanceof CircleShape) 		return colCaC(shape1, shape2);
+		if(shape2 instanceof RectangleShape) 	return colRaC(shape2, shape1);
+	}
+
+	return false;
+}
+
+class Shape {
+	constructor(x, y, fill='transparent', stroke='transparent', strokeWeight=1) {
+		this.pos = new Vector(x, y);
+		this.style = {
+			background: fill,
+			stroke: stroke,
+			strokeWeight: strokeWeight
+		};
+		this.drawOrigin = false;
+		this.speed = 0;
+		this.acceleration = 0;
+		this.isRunning = false;
+		this.origin = 'topLeft';
+	}
+
+	set fill(color) {this.style.background = color;}
+	set stroke(color) {this.style.stroke = color;}
+	set strokeWeight(int) {this.style.strokeWeight = int;}
+	set run(bool) {this.running = typeof bool == "boolean"?  bool : false;}
+
+	get background() {return this.style.background;}
+	get stroke() {return this.style.stroke;}
+	get strokeWeight() {return this.style.strokeWeight;}
+	get x() {return this.pos.x;}
+	get y() {return this.pos.y;}
+	get running() {return this.isRunning;}
+
+	showOrigin(bool) {this.drawOrigin = typeof bool == "boolean"?  bool : false;}
+
+	setOrigin(newOrigin) {
+		this.origin = originArr.indexOf(newOrigin)? newOrigin : 'topLeft';
+	}
+
+	setPosition(x, y=null) {
+		if(!y && x instanceof Vector) {
+			this.pos.x = x.x;
+			this.pos.y = x.y;
+		} else {
+			this.pos.x = x;
+			this.pos.y = y;
+		}
+	}
+
+	move(x, y) {
+		let speed = this.speed + (this.running? this.acceleration : 0);
+		if(!y && x instanceof Vector) {
+			this.pos.x += x.x * speed;
+			this.pos.y += x.y * speed;
+		} else {
+			this.pos.x += x * speed;
+			this.pos.y += y * speed;
+		}
+	}
+}
+
+class RectangleShape extends Shape {
+	constructor(x, y, w, h, fill='black', stroke='transparent', strokeWeight=1) {
+		super(x, y, fill, stroke, strokeWeight);
+		this.dim = new Vector(w, h);
+	}
+
+	get width() {return this.dim.x;}
+	get height() {return this.dim.y;}
+	
+	set width(newWidth) {this.dim.x = newWidth;}
+	set height(newHeight) {this.dim.y = newHeight;}
+
+	draw() {
+		fill(this.background);
+		stroke(this.stroke);
+		strokeWeight(this.strokeWeight);
+		
+		let offset = getOffsetVector(this);
+
+		fillRect(this.x-offset.x, this.y-offset.y, this.width, this.height);
+		line(this.x-offset.x-this.strokeWeight/2, this.y-offset.y, this.x-offset.x+this.width+this.strokeWeight/2, this.y-offset.y);
+		line(this.x-offset.x-this.strokeWeight/2, this.y-offset.y+this.height, this.x-offset.x+this.width+this.strokeWeight/2, this.y-offset.y+this.height);
+		line(this.x-offset.x, this.y-offset.y, this.x-offset.x, this.y-offset.y+this.height);
+		line(this.x-offset.x+this.width, this.y-offset.y, this.x-offset.x+this.width, this.y-offset.y+this.height);
+
+		if(this.drawOrigin) {
+			fill('#0F8');
+			noStroke();
+			circle(this.x, this.y, 2);
+		}
+	}
+
+	hover() {
+		let offset = getOffsetVector(this);
+		let x1 = this.x - offset.x,
+			y1 = this.y - offset.y;
+		let x2 = x1 + this.width,
+			y2 = y1 + this.height;
+
+		return x1 <= mouseX && mouseX <= x2 && y1 <= mouseY && mouseY <= y2;
+	}
+}
+
+class CircleShape extends Shape {
+	constructor(x, y, r, fill='black', stroke='transparent', strokeWeight=1) {
+		super(x, y, fill, stroke, strokeWeight);
+		this.radius = r;
+		this.origin = "center";
+	}
+
+	get r() {return this.radius;}
+	set r(newRadius) {this.radius = newRadius;}
+
+	draw() {
+		fill(this.background);
+		stroke(this.stroke);
+		strokeWeight(this.strokeWeight);
+
+		let offset = getOffsetVector(this);
+
+		circle(this.x-offset.x, this.y-offset.y, this.r);
+
+		if(this.drawOrigin) {
+			fill('#0F8');
+			noStroke();
+			circle(this.x, this.y, 2);
+		}
+	}
+
+	hover() {
+		let offset = getOffsetVector(this);
+		return pow(mouseX - this.x-offset.x) + pow(mouseY - this.y-offset.y) < pow(this.r);
+	}
+}
+
+class TriangleShape extends Shape {
+	constructor(x, y, baseLength, baseTiltinDegree, triangleHeight, heightPosition, fill='black', stroke='transparent', strokeWeight=1) {
+		super(x, y, fill, stroke, strokeWeight);
+		this.baseLength = baseLength;
+		this.baseTilt = radians(baseTiltinDegree);
+		this.height = triangleHeight;
+		this.heightPos = heightPosition;
+		this.origin = "bottomLeft";
+
+		this.vertex = {
+			A: new Vector(0,0),
+			B: new Vector(0,0),
+			C: new Vector(0,0)
+		};
+		this.refreshVertexPos();
+	}
+
+	get A() {return this.vertex.A;}
+	get B() {return this.vertex.B;}
+	get C() {return this.vertex.C;}
+
+	draw() {
+		this.refreshVertexPos();
+
+		fill(this.background);
+		stroke(this.stroke);
+		strokeWeight(this.strokeWeight);
+
+		polyline(this.A.x, this.A.y, this.B.x, this.B.y, this.C.x, this.C.y, this.vertex.A.x, this.A.y);
+
+		if(this.drawOrigin) {
+			fill('#0F8');
+			noStroke();
+			circle(this.x, this.y, 2);
+		}
+	} 
+
+	refreshVertexPos() {
+		let vec = angleToVector(this.baseTilt);
+		this.vertex.A.set(this.x, this.y);
+		this.vertex.B.set(this.A.x+this.baseLength*vec.x, this.A.y+this.baseLength*vec.y);
+		let d = dist(this.A, this.B)/2;
+		let k = new Vector(this.A.x+d*vec.x, this.A.y+d*vec.y);
+		let angle = angleToVector(radians(degree(this.baseTilt) - 90));
+		this.vertex.C.set(k.x+this.height*angle.x, k.y+this.height*angle.y, 5);
+	}
+
+	hover() {
+		let triArea = abs((this.B.x-this.A.x)*(this.C.y-this.A.y) - (this.C.x-this.A.x)*(this.B.y-this.A.y));
+		let area1 = abs((this.A.x-mouseX)*(this.B.y-mouseY) - (this.B.x-mouseX)*(this.A.y-mouseY));
+		let area2 = abs((this.B.x-mouseX)*(this.C.y-mouseY) - (this.C.x-mouseX)*(this.B.y-mouseY));
+		let area3 = abs((this.C.x-mouseX)*(this.A.y-mouseY) - (this.A.x-mouseX)*(this.C.y-mouseY));
+		return area1 + area2 + area3 == triArea;
+	}
+}
+
+class Triangle extends TriangleShape {
+	constructor(x1, y1, x2, y2, x3, y3, fill='black', stroke='black', strokeWeight=1) {
+		let A = new Vector(x1, y1), B = new Vector(x2, y2), C = new Vector(x3, y3);
+		let baseTilt = degree(Math.atan2(B.y-A.y, B.x-A.x)), base = dist(A, B);
+		let atv = angleToVector(radians(baseTilt));
+		let k = new Vector(A.x+base/2*atv.x, A.y+base/2*atv.y);
+		let tHeight = dist(k,C), heightPosition = C.x-A.x;
+		super(A.x, A.y, base, baseTilt, tHeight, heightPosition, fill, stroke, strokeWeight);
+	}
+}
